65d0e486ffb193835b553cd3061f32310b13858e
Simon Zwick <simonzwick19@gmail.com>
2017-03-16 14:02:56 -0700

    closes #4

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,32 +2,37 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application
-{
+class Welcome extends Application {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct() {
+        parent::__construct();
+    }
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+    /**
+     * Homepage for our app
+     */
+    public function index() {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'homepage';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-		}
-		$this->data['authors'] = $authors;
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->all();
+        $authors = array();
+        foreach ($source as $record) {
+            $authors[] = array('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+        }
+        $this->data['authors'] = $authors;
 
-		$this->render();
-	}
+        $this->render();
+    }
+
+    public function shucks() {
+        $source = $this->quotes->get('2');
+        $this->data['pagebody'] = 'justone';
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->render();
+    }
 
 }
